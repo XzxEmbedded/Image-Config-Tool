0ef3cb06c541fd9a292e22b43fc38970700037a9
xzx <xuzhenxing@canaan-creative.com>
2018-03-15 18:21:17 +0800

    Update config-raspbian-img.sh

--- config-raspbian-img.sh
+++ config-raspbian-img.sh
@@ -4,20 +4,51 @@
 
 # Mount img file
 # Check offset value: fdisk -lu img-file
-mkdir ./mount
-sudo mount -t auto -o loop,offset=$((94208*512)) ./2017-11-29-raspbian-stretch-lite.img ./mount
-sleep 1
+mount_img() {
+    mkdir ./mount
+    sudo mount -t auto -o loop,offset=$((94208*512)) ./2017-11-29-raspbian-stretch-lite.img ./mount
+    sleep 1
+}
 
 # Git pyserial
-cd ./mount/home/pi
-git clone https://github.com/XzxEmbedded/pyserial.git
-sleep 1
+git_pyserial() {
+    cd ./mount/home/pi
+    git clone https://github.com/XzxEmbedded/pyserial.git
+    sleep 1
+}
 
 # Git miner-automate-test-scripts
-git clone https://github.com/XzxEmbedded/miner-automate-test-scripts.git
-sleep 1
+git_scripts() {
+    git clone https://github.com/XzxEmbedded/miner-automate-test-scripts.git
+    sleep 1
+}
 
 # Umount img file
-cd ../../../
-sudo umount ./mount
-rm -fr ./mount
+umount_img() {
+    cd ../../../
+    sudo umount ./mount
+    rm -fr ./mount
+}
+
+for conf in "$@"
+do
+    case $conf in
+        --mount)
+            mount_img
+            ;;
+        --serial)
+            git_pyserial
+            ;;
+        --scripts)
+            git_scripts
+            ;;
+        --umount)
+            umount_img
+            ;;
+        --all)
+            mount_img && git_pyserial && git_scripts && umount_img
+            ;;
+        *)
+            ;;
+    esac
+done
