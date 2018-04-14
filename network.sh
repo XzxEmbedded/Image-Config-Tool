58c40f7f9011a77d438eb1e2ff397e47cb64d008
xzx <xuzhenxing@canaan-creative.com>
2018-04-14 17:09:05 +0800

    Add router && dns config for network

--- network.sh
+++ network.sh
@@ -7,6 +7,10 @@ if [ "$?" -eq "0" ]; then
     sudo sed -i "39 a $interface" /etc/dhcpcd.conf
     ip=`cat /home/pi/network.conf | grep ip_address`
     sudo sed -i "40 a $ip" /etc/dhcpcd.conf
+    router=`cat /home/pi/network.conf | grep routers`
+    sudo sed -i "41 a $router" /etc/dhcpcd.conf
+    dns=`cat /home/pi/network.conf | grep domain`
+    sudo sed -i "42 a $dns" /etc/dhcpcd.conf
     rm /home/pi/network.conf
 
     sudo ifconfig eth0 down
